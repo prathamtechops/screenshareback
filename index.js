7f3e036d94913fd1ffe8159866c8e24a9530d0cf
Pratham-Techops <poddarpratham@gmail.com>
2023-12-25 07:40:12 +0530

    --

--- index.js
+++ index.js
@@ -6,73 +6,48 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://localhost:3002",
-    ],
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST"],
   },
 });
 
-const connectedPeers = new Map();
+const activeUsers = {};
 
 io.on("connection", (socket) => {
-  console.log("Connected: " + socket.id);
-  connectedPeers.set(socket.id, socket);
+  const userId = socket.id;
+  activeUsers[userId] = socket.id;
+
+  socket.on("user_connected", () => {
+    socket.broadcast.emit("user_connected", userId);
+    console.log("user connected");
+  });
+
+  socket.on("send-offer-to-admin", (offer) => {
+    socket.broadcast.emit("offer-from-user", offer, userId);
+    console.log("offer sent");
+  });
+
+  socket.on("send-answer-to-user", (answer, userId) => {
+    socket.to(userId).emit("answer-from-admin", answer);
+    console.log("answer sent");
+  });
+
+  socket.on("send-ice-candidate-to-admin", (candidate) => {
+    socket.broadcast.emit("ice-candidate-from-user", candidate, userId);
+    console.log("ice candidate sent");
+  });
+
+  socket.on("send-ice-candidate-to-user", (candidate, userId) => {
+    socket.to(userId).emit("ice-candidate-from-admin", candidate);
+    console.log("ice candidate sent");
+  });
 
   socket.on("disconnect", () => {
-    console.log("Disconnected: " + socket.id);
-    connectedPeers.delete(socket.id);
-  });
-
-  socket.on("send-offer-to-admin", (offer, fromId) => {
-    console.log("User: Sending offer to admin");
-    setTimeout(() => {
-      for (const [id, socketPeer] of connectedPeers.entries()) {
-        if (id !== socket.id) {
-          console.log("Server: Sending offer from user to admin");
-          socketPeer.emit("offer-from-user", offer, fromId);
-        }
-      }
-    }, 2000); // delay the emission of the offer-from-user event by 2 seconds
-  });
-
-  socket.on("send-answer-to-user", (answer, toId, fromId) => {
-    console.log("Admin: Sending answer to user");
-    const socketPeer = connectedPeers.get(toId);
-    if (socketPeer) {
-      console.log("Server: Sending answer from admin to user");
-      socketPeer.emit("answer-from-admin", answer, fromId);
-    } else {
-      console.error("No peer found with ID:", toId);
-    }
-  });
-
-  socket.on("send-ice-candidate-to-admin", (candidate, fromId) => {
-    console.log("User: Sending ICE candidate to admin");
-    for (const [id, socketPeer] of connectedPeers.entries()) {
-      if (id !== socket.id) {
-        console.log("Server: Sending ICE candidate from user to admin");
-        socketPeer.emit("ice-candidate-from-user", candidate, fromId);
-      }
-    }
-  });
-
-  socket.on("send-ice-candidate-to-user", (candidate, toId, fromId) => {
-    console.log("Admin: Sending ICE candidate to user");
-    const socketPeer = connectedPeers.get(toId);
-    if (socketPeer) {
-      console.log("Server: Sending ICE candidate from admin to user");
-      socketPeer.emit("ice-candidate-from-admin", candidate, fromId);
-    } else {
-      console.error("No peer found with ID:", toId);
-    }
+    delete activeUsers[userId];
+    socket.broadcast.emit("user_disconnected", userId);
   });
 });
 
-const PORT = 5000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(5000, () => {
+  console.log("Server listening on port 5000");
 });
